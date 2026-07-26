5d96f556bf954078ceda95f19feb55885b7b5622
Daksh-pal <pal.daksh22@gmail.com>
2026-07-26 16:02:28 +0530

    Fix CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,10 +7,12 @@ import connectDb from './config/database.js';
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true
-}));
+app.use(
+    cors({
+        origin: true,
+        credentials: true,
+    })
+);
 
 app.use(cookieParser());
 app.use(express.json());
